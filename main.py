e38a3fab24d76522c13a619fa99c59fea6de3bbd
Shiva-Kumarr-Thimmaraveni <153267857+Shiva-Kumarr-Thimmaraveni@users.noreply.github.com>
2024-07-06 12:58:11 +0000

    Added Logic for Single Prediction

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import streamlit as st
 
+
+# Set desired size in pixels
+width_pixels = 400
+height_pixels = 200
+
+# Convert pixels to inches (assuming default DPI of 100)
+dpi = 100
+width_inches = width_pixels / dpi
+height_inches = height_pixels / dpi 
+
+
 # Importing the dataset
 dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:, :-1].values
@@ -29,35 +40,34 @@ years_experience_predicted = regressor.predict([[years_experience]])
 st.title("Predicting Salary by Experience")
 if st.sidebar.button('Submit'):
     years_experience_predicted = regressor.predict([[years_experience]])
+    st.success(f'Predicted Salary for Given Experience is {years_experience_predicted[0].round()} $')
+    
 
-    st.subheader(f'Plot of Sine Wave for {years_experience_predicted} Years of Experience')
-
-
-
-# Visualising the Training set results
-plt.scatter(X_train, y_train, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Training set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
 
 # Create a plot
-fig, ax = plt.subplots(figsize=(4, 3))
+fig, ax = plt.subplots(figsize=(width_inches, height_inches))
 ax.plot(X_train, regressor.predict(X_train), color = 'blue')
 ax.scatter(X_train, y_train, color = 'red')
 ax.set(title='Salary vs Experience (Training set)', xlabel='Years of Experience', ylabel='Salary')
 st.pyplot(fig)
 
-# Visualising the Test set results
-plt.scatter(X_test, y_test, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Test set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
-
 # Create a plot
-fig2, ax2 = plt.subplots(figsize=(4, 3))
+fig2, ax2 = plt.subplots(figsize=(width_inches, height_inches))
 ax2.plot(X_train, regressor.predict(X_train), color = 'blue')
 ax2.scatter(X_test, y_test, color = 'red')
 ax2.set(title='Salary vs Experience (Test set)', xlabel='Years of Experience', ylabel='Salary')
 st.pyplot(fig2)
+
+# # Visualising the Training set results if required in graph plot.show()
+# plt.scatter(X_train, y_train, color = 'red')
+# plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+# plt.title('Salary vs Experience (Training set)')
+# plt.xlabel('Years of Experience')
+# plt.ylabel('Salary')
+
+# # Visualising the test set results if required in graph plot.show()
+# plt.scatter(X_test, y_test, color = 'red')
+# plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+# plt.title('Salary vs Experience (Test set)')
+# plt.xlabel('Years of Experience')
+# plt.ylabel('Salary')
